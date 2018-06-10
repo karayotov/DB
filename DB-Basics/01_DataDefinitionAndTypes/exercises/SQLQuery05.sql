298baf480667f8f011be60dc02298611fed6522e
karayotov <32219166+karayotov@users.noreply.github.com>
2018-06-10 14:48:35 +0300

    DB table relationship exercises

---
+++ DB-Basics/01_DataDefinitionAndTypes/exercises/SQLQuery05.sql
@@ -0,0 +1,90 @@
+CREATE DATABASE OnlineStore
+
+USE OnlineStore
+
+CREATE TABLE Orders(
+	OrderID INT NOT NULL,
+	CustomerID INT NOT NULL
+	)
+
+
+CREATE TABLE Customers(
+	CustomerID INT NOT NULL,
+	[Name] VARCHAR(50),
+	Birthday DATE,
+	CityID INT NOT NULL
+	)
+
+CREATE TABLE Cities(
+	CityID INT NOT NULL,
+	[Name] VARCHAR(50)
+)
+
+
+CREATE TABLE OrderItems(
+	OrderID INT NOT NULL,
+	ItemID INT NOT NULL
+)
+
+
+CREATE TABLE Items(
+	ItemID INT NOT NULL,
+	[Name] VARCHAR(50),
+	ItemTypeID INT NOT NULL
+)
+
+
+CREATE TABLE ItemTypes(
+	ItemTypeID INT NOT NULL,
+	[Name] VARCHAR(50)
+)
+
+
+ALTER TABLE Orders
+	ADD CONSTRAINT PK_Orders
+	PRIMARY KEY (OrderID)
+
+ALTER TABLE Customers
+	ADD CONSTRAINT PK_Customers
+	PRIMARY KEY (CustomerID)
+
+ALTER TABLE Orders
+	ADD CONSTRAINT FK_OrdersCustomers
+	FOREIGN KEY(CustomerID)
+	REFERENCES Customers(CustomerID)
+
+ALTER TABLE Cities
+	ADD CONSTRAINT PK_Cities
+	PRIMARY KEY (CityID)
+
+ALTER TABLE Customers
+	ADD CONSTRAINT FK_Cities 
+	FOREIGN KEY (CityID)
+	REFERENCES Cities(CityID)
+
+ALTER TABLE Items
+	ADD CONSTRAINT PK_Items
+	PRIMARY KEY(ItemID)
+
+ALTER TABLE ItemTypes
+	ADD CONSTRAINT PK_ItemTypes
+	PRIMARY KEY (ItemTypeID)
+
+ALTER TABLE Items
+	ADD CONSTRAINT FK_ItemTypes
+	FOREIGN KEY (ItemTypeID)
+	REFERENCES ItemTypes(ItemTypeID)
+
+ALTER TABLE OrderItems
+	ADD CONSTRAINT PK_OrderItems
+	PRIMARY KEY (OrderID, ItemID)
+
+ALTER TABLE OrderItems
+	ADD CONSTRAINT FK_Orders
+	FOREIGN KEY (OrderID)
+	REFERENCES Orders(OrderID)
+
+ALTER TABLE OrderItems
+	ADD CONSTRAINT FK_Items
+	FOREIGN KEY (ItemID)
+	REFERENCES Items(ItemID)
